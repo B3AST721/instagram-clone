4bb6d7188781fa08aff7719073b53b466e3c046b
Jaheel <jaheelwarr@gmail.com>
2022-05-18 14:23:22 -0600

    added header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { React } from 'react';
+import Post from './components/Post';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+        <img 
+          className='logo'
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png?20160616034027'
+          alt=''
+        />
+      </div>
+      {/* Header */}
+
+      {/* Post */}
+      <Post />
+      {/* Post */}
     </div>
   );
 }
---
+++ src/components/Post.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Post() {
+  return (
+    <div>
+        <h3>Username</h3>
+        
+    </div>
+  )
+}
+
+export default Post
